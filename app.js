20cfb736e564789c175e8e9aa6c8474f683f90b0
Marco Seghesso <mseghesso@gmail.com>
2021-07-16 08:10:25 +1200

    Counter set and colors set as well

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
-let counter = 0;
+//Set up counter
+let count = 0;
 
+//Select Value and Buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener(`click`, function (e) {
+        const btnClasses = e.currentTarget.classList;
+        if (btnClasses.contains("decrease")){
+            count--;
+        }
+        else if (btnClasses.contains("increase")){
+            count++;
+        }
+        else {
+            count = 0;
+        }
+        if(count > 0){
+            value.style.color = "green"
+        }
+        if(count > 0){
+            value.style.color = "green"
+        }
+        else if(count < 0) {
+            value.style.color = "red"
+        }
+        else {
+            value.style.color = "#222"
+        }
+        value.textContent = count;
+    });
+});
